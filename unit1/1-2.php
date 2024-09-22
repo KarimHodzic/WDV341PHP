3ceb93642856ef4f7b4cbc726c8503513e0c48f3
Karim Hodzic <Karim.Hodzic@gmail.com>
2024-09-21 20:35:27 -0500

    Adding 1-2 Homework
    Adding 1-2 Download Git Client & Create a Repository account homework

---
+++ unit1/1-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1-2</title>
+</head>
+<body>
+    <h1>WDV341 Unit 1-2 Download Git Client & Create a Repository account</h1>
+    <h3>Research and define the following Git related terms on an HTML or Word document.</h3>
+    <ul>
+        <li>Version Control Software: Version control software keeps track of every modification to the code in a special kind of database. If a mistake is made, developers can turn back the clock and compare earlier versions of the code to help fix the mistake while minimizing disruption to all team members.</li>
+        <li>Add: The git add command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit.</li>
+        <li>Commit: The git commit command captures a snapshot of the project's currently staged changes.</li>
+        <li>Push: The git push command is used to upload local repository content to a remote repository.</li>
+        <li>Pull: The git pull command is used to fetch and download content from a remote repository and immediately update the local repository to match that content</li>
+        <li>Clone: Git clone is a Git command line utility which is used to target an existing repository and create a clone, or copy of the target repository.</li>
+    </ul>
+</body>
+</html>
